7dd1c630564e6e2160c5d46fd60a783ee2fb7c9c
CABS72030202 <sebastien.cabana@uqtr.ca>
2024-09-15 00:52:10 -0400

    Added comments

--- src/main.c
+++ src/main.c
@@ -27,26 +27,24 @@ char* Response(char*);                               // Simulates communication 
 
 // Global variables and constants
 const int ERROR = 1;
-const int OK = 0;
-int r_station_pass[5];
-int g_station_pass[5];
-int b_station_pass[5];
-int y_station_pass[5];
-int p_station_pass[5];
-int blackbox_pass[5];
-Color curr_station;
-Color next_station;
+const int OK = 0;    
+int r_station_pass[5];      // Passenger count for each destination still waiting at station RED since last visit
+int g_station_pass[5];      // Passenger count for each destination still waiting at station GREEN since last visit
+int b_station_pass[5];      // Passenger count for each destination still waiting at station BLUE since last visit
+int y_station_pass[5];      // Passenger count for each destination still waiting at station YELLOW since last visit
+int p_station_pass[5];      // Passenger count for each destination still waiting at station PURPLE since last visit
+int blackbox_pass[5];       // Passenger count for each destination onboard
+Color curr_station;         // Current station (NONE if not properly connected to one)
 
 // Prototypes
-void Initialize();
-char* Print_Color(Color);
-void Print_Count(int*);
-int Update_Station_Color();
-int Update_Station_Count();
-int Update_Blackbox_Count();
-char* Format_Str(char*, Color, int);
-int Drop_Passengers();
-int Load_Passengers(Color, int);
+char* Color_To_String(Color);           // Get string format from enum Color
+void Print_Count(int*);                 // Print the number of passenger going to each station from an array of size 5
+int Update_Station_Color();             // Get current station color
+int Update_Station_Count();             // Get current station passenger count
+int Update_Blackbox_Count();            // Get current number of passenger onboard
+char* Format_Str(char*, Color, int);    // 
+int Drop_Passengers();                  // Drop all corresponding passengers onboard to current station
+int Load_Passengers(Color, int);        // Load a specific number of passengers going to a specific station
 
 int main() {
     printf("----- start main -----\n\n");
@@ -60,7 +58,7 @@ int main() {
     return OK;
 }
 
-char* Print_Color(Color c) {
+char* Color_To_String(Color c) {
     switch (c)
     {
     case RED:
@@ -76,7 +74,7 @@ char* Print_Color(Color c) {
     case NONE:
         return "NONE";
     default:
-        printf("\nERROR. INVALID COLOR | main.c | Print_Color\n\n");
+        printf("\nERROR. INVALID COLOR | main.c | Color_To_String\n\n");
         return NULL;
     }
 }
@@ -84,12 +82,17 @@ void Print_Count(int* array) {
     printf("Red:%i\nGreen:%i\nBlue:%i\nYellow:%i\nPurple:%i\n", array[0], array[1], array[2], array[3], array[4]);
 }
 int Update_Station_Color() {
+    // Get message from blackbox using predefined INFO function
     char* response = Response("INFO:C\n");
+
+    // Detect and print error
     if (response[0] == 'E') {
         printf("Cannot update current station color : %s", response);
         curr_station = NONE;
         return ERROR;
     }
+
+    // Decode message and set current station
     char c = response[3];
     switch (c)
     {
@@ -114,9 +117,14 @@ int Update_Station_Color() {
     }
 }
 int Update_Station_Count() {
+    // Get message from blackbox using predefined INFO function
     char* response = Response("INFO:S\n");
+
+    // Detect and print error
     if (response == NULL || Update_Station_Color() == ERROR)
         return ERROR;
+
+    // Decode message and update passenger count array for current station
     switch (curr_station)
     {
     case RED:
@@ -147,11 +155,16 @@ int Update_Station_Count() {
     }
 }
 int Update_Blackbox_Count() {
+    // Get message from blackbox using predefined INFO function
     char* response = Response("INFO:B\n");
+
+    // Detect and print error
     if (response == NULL || response[0] == 'E') {
         printf("Cannot update blackbox passenger count : %s", response);
         return ERROR;
     }
+
+    // Decode message and update passenger count array for blackbox
     for (int i = 0, j = 3; j < strlen(response); i++, j += 2)
         blackbox_pass[i] = response[j] - '0';
     return OK;
@@ -164,27 +177,27 @@ char* Format_Str(char* comm, Color c, int nb) {
     case RED:
         size += 3;
         str = (char*)malloc(size * sizeof(char));
-        sprintf(str, "%s:%s:%i\n", comm, Print_Color(c), nb);
+        sprintf(str, "%s:%s:%i\n", comm, Color_To_String(c), nb);
         break;
     case GREEN:
         size += 5;
         str = (char*)malloc(size * sizeof(char));
-        sprintf(str, "%s:%s:%i\n", comm, Print_Color(c), nb);
+        sprintf(str, "%s:%s:%i\n", comm, Color_To_String(c), nb);
         break;
     case BLUE:
         size += 4;
         str = (char*)malloc(size * sizeof(char));
-        sprintf(str, "%s:%s:%i\n", comm, Print_Color(c), nb);
+        sprintf(str, "%s:%s:%i\n", comm, Color_To_String(c), nb);
         break;
     case YELLOW:
         size += 6;
         str = (char*)malloc(size * sizeof(char));
-        sprintf(str, "%s:%s:%i\n", comm, Print_Color(c), nb);
+        sprintf(str, "%s:%s:%i\n", comm, Color_To_String(c), nb);
         break;
     case PURPLE:
         size += 6;
         str = (char*)malloc(size * sizeof(char));
-        sprintf(str, "%s:%s:%i\n", comm, Print_Color(c), nb);
+        sprintf(str, "%s:%s:%i\n", comm, Color_To_String(c), nb);
         break;
     default:
         printf("\nERROR. INVALID COLOR | main.c | Format_Str\n\n");
@@ -193,29 +206,43 @@ char* Format_Str(char* comm, Color c, int nb) {
     return str;
 }
 int Drop_Passengers() {
+    // Detect and abort if error detected
     if(Update_Blackbox_Count() || Update_Station_Color())
         return ERROR;
+
+    // Get message from blackbox using predefined SEND function
     char* response = Response(Format_Str("SEND", curr_station, blackbox_pass[curr_station]));
+
+    // Detect and print error
     if(response[0] == 'E') {
         printf("Cannot send passengers to station : %s", response);
         return ERROR;       
     }
+
+    // Print confirmation message if successful
     else {
-        printf("\nSuccessfully sent %i passengers to %s station\n", blackbox_pass[curr_station], Print_Color(curr_station));
+        printf("\nSuccessfully sent %i passengers to %s station\n", blackbox_pass[curr_station], Color_To_String(curr_station));
         blackbox_pass[curr_station] = 0;
         return OK;
     }
 }
 int Load_Passengers(Color c, int nb) {
+    // Detect and abort if error detected
     if(Update_Blackbox_Count() || Update_Station_Color())
         return ERROR;
+
+    // Get message from blackbox using predefined TAKE function
     char* response = Response(Format_Str("TAKE", c, nb));
+
+    // Detect and print error
     if(response[0] == 'E') {
-        printf("Cannot take %i passengers going to %s from %s : %s", nb, Print_Color(c), Print_Color(curr_station), response);
+        printf("Cannot take %i passengers going to %s from %s : %s", nb, Color_To_String(c), Color_To_String(curr_station), response);
         return ERROR;       
     }
+
+    // Print confirmation message if successful
     else {
-        printf("\nSuccessfully took %i passengers going to %s from %s\n", nb, Print_Color(c), Print_Color(curr_station));
+        printf("\nSuccessfully took %i passengers going to %s from %s\n", nb, Color_To_String(c), Color_To_String(curr_station));
         blackbox_pass[c] += nb;
         return OK;
     }
