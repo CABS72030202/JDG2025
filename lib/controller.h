fd889281b2afdb67cb0a8c94ba6ca7c5dfb40517
Sébastien Cabana <156612899+CABS72030202@users.noreply.github.com>
2024-11-14 16:12:03 -0500

    Defined button actions

--- lib/controller.h
+++ lib/controller.h
@@ -16,27 +16,27 @@
 #define GAMEPAD_PATH "/dev/input/js0"
 
 // Digital button IDs
-#define A_BUTTON 0
-#define B_BUTTON 1
-#define X_BUTTON 2
-#define Y_BUTTON 3
-#define LB_BUTTON 4
-#define RB_BUTTON 5
-#define BACK_BUTTON 6
-#define START_BUTTON 7
-#define HOME_BUTTON 8
-#define L_STICK_PRESS 9
-#define R_STICK_PRESS 10
+#define A_BUTTON 0                  // 
+#define B_BUTTON 1                  // 
+#define X_BUTTON 2                  // 
+#define Y_BUTTON 3                  // Change constant speed
+#define LB_BUTTON 4                 // Control back left wheel
+#define RB_BUTTON 5                 // Control back right wheel
+#define BACK_BUTTON 6               // Select previous robot
+#define START_BUTTON 7              // Select next robot
+#define HOME_BUTTON 8               // Add station in queue
+#define L_STICK_PRESS 9             // 
+#define R_STICK_PRESS 10            // 
 
 // Analog button IDs (axis)
-#define LT_BUTTON 2
-#define RT_BUTTON 5
-#define L_STICK_X 0
-#define L_STICK_Y 1
-#define R_STICK_X 3
-#define R_STICK_Y 4
-#define CROSS_X 6
-#define CROSS_Y 7
+#define LT_BUTTON 2                 // Control front left wheel (variable speed)
+#define RT_BUTTON 5                 // Control front right wheel (variable speed)
+#define L_STICK_X 0                 // 
+#define L_STICK_Y 1                 // 
+#define R_STICK_X 3                 // 
+#define R_STICK_Y 4                 // 
+#define CROSS_X 6                   // Rotate using one wheel
+#define CROSS_Y 7                   // Drive with both wheel
 
 // Analog ranges
 #define ANAL_SHIFT_VAL 32767        
