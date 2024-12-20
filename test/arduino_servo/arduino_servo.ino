c62fd6f7e768c51a3fc12785d7fd7515e68cda12
Sébastien Cabana <156612899+CABS72030202@users.noreply.github.com>
2024-12-20 08:52:10 -0500

    Update servo sketch to test grip gears

--- test/arduino_servo/arduino_servo.ino
+++ test/arduino_servo/arduino_servo.ino
@@ -2,42 +2,68 @@
 // Created on: 2024-12-14
 // Author: Sebastien Cabana
 
-/* 
- * 
- */
-
 // Includes
 #include <stdlib.h>
 #include <string.h>
 #include <Servo.h>
 
 // Define pins
-#define SERVO_PIN 9
+#define GRIP_PIN 9
+#define HEIGHT_PIN 10
 
-// Global constants 
-#define ARM_ANGLE_UP 20
-#define ARM_ANGLE_DOWN 60
+// Global constants
+#define ANGLE_STEP 2
+#define OPEN_BOUND 0
+#define CLOSE_BOUND 180
+#define UP_BOUND 200
+#define DOWN_BOUND 0
 
 // Global variables
-int arm_state = 0;              // 0 is DOWN, 1 is UP
-Servo myServo;
+int grip_angle = 5;      // Starting angle
+int height_angle = 20;
+Servo grip;
+Servo height;
 
 // Prototypes
-void Arm();
+void Open();
+void Close();
+void Up();
+void Down();
 
 void setup() {
-  myServo.attach(SERVO_PIN);
+  grip.attach(GRIP_PIN);
+  grip.write(grip_angle);
+  height.attach(HEIGHT_PIN);
+  height.write(height_angle);
 }
 
 void loop() {
-  Arm();
-  delay(2000);
-  arm_state = arm_state ^ 1;
+  for(int i = 0; i < 10; i++) {
+    Close();
+    Delay(500);
+  }
+  for(int i = 0; i < 10; i++) {
+    Open();
+    Delay(500);
+  }
 }
 
-void Arm() {
-  if(arm_state)
-    myServo.write(ARM_ANGLE_UP);
-  else
-    myServo.write(ARM_ANGLE_DOWN);
+void Open() {
+  if((grip_angle - ANGLE_STEP) < OPEN_BOUND)
+    return;
+  grip_angle -= ANGLE_STEP;
+  grip.write(grip_angle);
+}
+
+void Close() {
+  if((grip_angle + ANGLE_STEP) > CLOSE_BOUND)
+    return;
+  grip_angle += ANGLE_STEP;
+  grip.write(grip_angle);
+}
+
+void Up() {
+}
+
+void Close() {
 }
