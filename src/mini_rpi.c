a21b2d167da4a87fdb106de8869463d40cec949c
CABS72030202 <sebastien.cabana@uqtr.ca>
2024-12-30 00:23:17 -0500

    Error management in mini rpi program

--- src/mini_rpi.c
+++ src/mini_rpi.c
@@ -5,29 +5,30 @@
 #include "../lib/rpi_bluetooth.h"
 
 int main() {
+START:
      // Open the serial port
     int uart_fd = serialOpen(UART, BAUD_RATE);
     if (uart_fd < 0) {
         fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
-        return 1;
+        goto ERR;
     }
 
     // Initialize wiringPi
     if (wiringPiSetup() == -1) {
         fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));
-        return 1;
+        goto ERR;
     }
 
     // Initialize Bluetooth communication as client
     int client_sock = bt_init(0, SERVER_ADR);
-    if (client_sock < 0) return 1;
+    if (client_sock < 0) goto ERR;
 
     while(1) {
         // Receive a string
         char buffer[BUFFER_SIZE];
         if (bt_receive(client_sock, buffer, BUFFER_SIZE) < 0) {
             close(client_sock);
-            return 1;
+            goto ERR;
         }
 
         // Transmit message via UART
@@ -35,6 +36,9 @@ int main() {
             serialPutchar(uart_fd, buffer[i]);
     }
 
+ERR:
+    serialClose(uart_fd);
     close(client_sock);
+    goto START;
     return 0;
 }
