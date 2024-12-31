8d8d5dab22f692c223c49cb1a4be7ba4a4c01561
Sébastien Cabana <156612899+CABS72030202@users.noreply.github.com>
2024-12-31 14:00:42 -0500

    Improve buzzer test timing management while keeping Pikmin sound effect

--- test/arduino_buzzer/arduino_buzzer.ino
+++ test/arduino_buzzer/arduino_buzzer.ino
@@ -8,16 +8,19 @@ int melody[NOTE_COUNT] = {
   NOTE_DS6, NOTE_E6, NOTE_CS6, NOTE_C6
 };
 
-int noteDurations[NOTE_COUNT] = {
-  SIXTEENTH_NOTE, DOT(EIGHTH_NOTE), SIXTEENTH_NOTE, DOT(QUARTER_NOTE)
+float noteDurations[NOTE_COUNT] = {
+  125, 500, 125, 1250
 };
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("-----");
   // iterate over the notes of the melody:
   for (int thisNote = 0; thisNote < NOTE_COUNT; thisNote++) {
 
     // to calculate the note duration, take one second divided by the note type.
-    int noteDuration = (10000 * noteDurations[thisNote]) / TEMPO;
+    int noteDuration = MSPB * noteDurations[thisNote];
+    Serial.println(noteDuration);
     tone(8, melody[thisNote], noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
--- test/arduino_buzzer/durations.h
+++ test/arduino_buzzer/durations.h
@@ -1,9 +1,10 @@
-#define TEMPO         80
-#define QUARTER_NOTE  7
+#define TEMPO         60000
+#define QUARTER_NOTE  1
 
+#define MSPB                60000 / TEMPO
 #define HALF_NOTE           2.0 * QUARTER_NOTE
 #define WHOLE_NOTE          2.0 * HALF_NOTE
 #define EIGHTH_NOTE         0.5 * QUARTER_NOTE
 #define SIXTEENTH_NOTE      0.5 * EIGHTH_NOTE
 
-const int DOT(const int note) {return 1.5 * note;}
+const float DOT(const float note) {return 1.5 * note;}
