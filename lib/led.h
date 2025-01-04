99c56dbf6b0106c582b4d8156d69e0f3b2f2168b
Sébastien Cabana <156612899+CABS72030202@users.noreply.github.com>
2025-01-04 17:33:48 -0500

    Add colors

--- lib/led.h
+++ lib/led.h
@@ -41,12 +41,23 @@
 #define LED_BRIGHTNESS  255      // Maximum brightness
 #define LED_CHANNEL     0        // Channel for controlling LEDs
 
+// Color IDs
+#define RED_ID      0
+#define GREEN_ID    1
+#define BLUE_ID     2
+#define YELLOW_ID   3
+#define PURPLE_ID   4
+#define WHITE_ID    5
+#define ORANGE_ID   6
+#define PINK_ID     7
+
 // Global variables
 extern ws2811_t ledstring;
 
 // Function Prototypes
 int LED_Init();                             // Initialize the LEDs
-void Set_LED_Color(int, int, int, int);     // Set the color of a specific LED with RGB format
+void Set_LED_RGB(int, int, int, int);       // Set the color of a specific LED with RGB format
+void Set_LED_Color(int, int);               // Set the color of a specific LED from color ID
 void Clear_LEDs();                          // Turn off all LEDs
 
 #endif
